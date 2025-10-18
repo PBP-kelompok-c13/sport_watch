68f4907022a5e9af07fe754a2701352124b80a0f
mfadhilfajar-oss <m.fadhilfajar@gmail.com>
2025-10-18 16:23:07 +0700

    sambungin url routing untuk scoreboard

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'judul': 'Scoreboard',
+        'tim': [
+            {'nama': 'Team A', 'skor': 78},
+            {'nama': 'Team B', 'skor': 65},
+            {'nama': 'Team C', 'skor': 90},
+        ]
+    }
+    return render(request, 'scoreboard/index.html', context)
--- sport_watch/urls.py
+++ sport_watch/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('portal_berita.urls')),
+    path('scoreboard/', include('scoreboard.urls')), 
 ]
