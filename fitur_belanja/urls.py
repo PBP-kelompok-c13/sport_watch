647c2e379e0db46ee51a629af66c71e52ab2261b
keseter <worang19@gmail.com>
2025-10-24 22:57:20 +0700

    Assisted kode dzaki untuk fitur_belanja

--- fitur_belanja/urls.py
+++ fitur_belanja/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("clear/", views.clear_cart, name="clear_cart"),
     path("update/", views.update_qty, name="update_qty"),
     path("remove/", views.remove_item, name="remove_item"),
+    path("checkout/", views.checkout, name="checkout"), 
 ]
